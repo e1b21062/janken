751d227fb5363d41a217391b766ab5ad32a4d29d
e1b21062 <e1b21062@st.oit.ac.jp>
2023-10-08 11:38:53 +0900

    じゃんけんゲームの完成

---
+++ src/main/java/oit/is/z1976/kaizi/janken/controller/JankenController.java
@@ -0,0 +1,57 @@
+package oit.is.z1976.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class JankenController {
+
+  @PostMapping("/janken")
+  public String jankenmain(@RequestParam String name, ModelMap model) {
+    String setname = name;
+    int index = 1;
+    model.addAttribute("setname", setname);
+    model.addAttribute("index", index);
+    return "janken.html";
+  }
+
+  @GetMapping("gu")
+  public String jankengu(ModelMap model) {
+
+    String ihand = "Gu";
+    String cphand = "Gu";
+    String result = "Draw";
+    model.addAttribute("ihand", ihand);
+    model.addAttribute("cphand", cphand);
+    model.addAttribute("result", result);
+    return "janken.html";
+  }
+
+  @GetMapping("choki")
+  public String jankenchoki(ModelMap model) {
+
+    String ihand = "Choki";
+    String cphand = "Gu";
+    String result = "You Lose";
+    model.addAttribute("ihand", ihand);
+    model.addAttribute("cphand", cphand);
+    model.addAttribute("result", result);
+    return "janken.html";
+  }
+
+  @GetMapping("pa")
+  public String jankenPa(ModelMap model) {
+
+    String ihand = "Pa";
+    String cphand = "Gu";
+    String result = "You Win!";
+    model.addAttribute("ihand", ihand);
+    model.addAttribute("cphand", cphand);
+    model.addAttribute("result", result);
+    return "janken.html";
+  }
+
+}
